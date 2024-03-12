7995ef9296d81ddf736120f6974e8d3344ed2ef1
wdolcine <dolcinemarcarthur01@gmail.com>
2024-03-12 08:56:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/flashcard_app/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.flashcard_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        flashcardAnswer.visibility = View.INVISIBLE
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+        flashcardAnswer.setOnClickListener {
+            flashcardQuestion.visibility = View.VISIBLE
+            flashcardAnswer.visibility = View.INVISIBLE
+        }
+    }
+}
