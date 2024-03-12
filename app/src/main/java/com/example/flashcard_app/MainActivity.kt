9f71f389862ed5bcfa5bedb54ed98dc4aba39d15
wdolcine <dolcinemarcarthur01@gmail.com>
2024-03-12 16:26:34 -0400

    update

--- app/src/main/java/com/example/flashcard_app/MainActivity.kt
+++ app/src/main/java/com/example/flashcard_app/MainActivity.kt
@@ -11,14 +11,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val flashcardAnswer2 = findViewById<TextView>(R.id.flashcard_answer2)
+        val flashcardAnswer3 = findViewById<TextView>(R.id.flashcard_answer3)
+
         flashcardAnswer.visibility = View.INVISIBLE
+        flashcardAnswer2.visibility = View.INVISIBLE
+        flashcardAnswer3.visibility = View.INVISIBLE
+
         flashcardQuestion.setOnClickListener {
-            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardQuestion.visibility = View.VISIBLE
             flashcardAnswer.visibility = View.VISIBLE
+            flashcardAnswer2.visibility = View.VISIBLE
+            flashcardAnswer3.visibility = View.VISIBLE
+
         }
         flashcardAnswer.setOnClickListener {
-            flashcardQuestion.visibility = View.VISIBLE
-            flashcardAnswer.visibility = View.INVISIBLE
+            findViewById<View>(R.id.flashcard_answer).setBackgroundColor(resources.getColor(R.color.green, null))
+            findViewById<View>(R.id.flashcard_answer2).setBackgroundColor(resources.getColor(R.color.white, null))
+
         }
+        flashcardAnswer2.setOnClickListener {
+            findViewById<View>(R.id.flashcard_answer).setBackgroundColor(resources.getColor(R.color.green, null))
+            findViewById<View>(R.id.flashcard_answer2).setBackgroundColor(resources.getColor(R.color.red, null))
+
+        }
+
     }
 }
